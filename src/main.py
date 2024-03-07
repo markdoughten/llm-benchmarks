a339a9d04945a5fdaf0a9ba2fe11d30199604484
Mark Doughten <doughtem@oregonstate.edu>
2024-03-06 21:54:11 -0500

    working on loops

--- src/main.py
+++ src/main.py
@@ -5,31 +5,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 
-def run(models, questions, gpu, graph, loops=1, kind='bar'):
-   
+def run(models, questions, gpu=True, write=False, graph=True, asks=1, display=['bar']):
+    
     # ask the models the prompts 
     performances = []
+    num_asks = asks
     for question in questions:
-        while loops != 0: 
+        while asks != 0: 
             for model in models:
                 performances.append(run_model(model, gpu, question))
-            loops -=1  
+            asks -=1  
         
     # calculate metrics 
     metrics = calculate(performances)
     metrics = pd.DataFrame(metrics)
    
     if graph: 
-        # plot time and count 
-        plot(metrics, 'Seconds', ['total_duration', 'load_duration', 'prompt_eval_duration', 'eval_duration'], gpu, kind)
-        plot(metrics, 'Count', ['prompt_eval_count', 'eval_count'], gpu, kind)
-        plot(metrics, 'Token per Second', ['prompt_eval_per_second',  'eval_per_second', 'total_per_second'], gpu, kind)
-    
+        for kind in display: 
+            # plot time and count 
+            plot(metrics, 'Seconds', ['total_duration', 'load_duration', 'prompt_eval_duration', 'eval_duration'], gpu, kind, write, num_asks)
+            plot(metrics, 'Count', ['prompt_eval_count', 'eval_count'], gpu, kind, write, num_asks)
+            plot(metrics, 'Token per Second', ['prompt_eval_per_second',  'eval_per_second', 'total_per_second'], gpu, kind, write, num_asks)
+     
     return metrics
 
 def run_model(model, gpu, prompt):
 
-    output = {'prompt': prompt}
+    output = {'prompt': prompt, 'gpu': gpu}
 
     if gpu:
         data = ollama.generate(model=model, prompt=prompt, options={'main_gpu':0, 'num_gpu':33})
@@ -53,9 +55,10 @@ def calculate(performances):
 
     return performances
     
-def plot(metrics, units, keys, gpu, kind):
+def plot(metrics, units, keys, gpu, kind, write, asks):
    
     keys.append('model')
+    info = f"Q:{len(metrics['prompt'].unique())}, A:{asks}"
     metrics = metrics[keys] 
 
     fig, ax = plt.subplots(figsize=(8, 8), dpi=96)
@@ -64,25 +67,36 @@ def plot(metrics, units, keys, gpu, kind):
          metrics.set_index('model').plot(kind=kind, ax=ax, rot=0)
     else:
         metrics.set_index('model').groupby(level=0).median().plot(kind=kind, ax=ax, rot=0)
-
+    
     if gpu:
-        plt.title('GPU')
+        plt.title(f'GPU: {info}')
     else: 
-        plt.title('CPU')    
+        plt.title('CPU: {info}')    
      
     plt.xlabel('Model')
     plt.ylabel(units)
     plt.tight_layout()
-    plt.show()
-        
+
+    if write:
+       save(chart=True, gpu=gpu, plt=plt)
+
+    #plt.show()
+    
     return
 
-def save(df, gpu):
+def save(df=None, gpu=None, chart=False, plt=None):
+    
     if gpu:
         prefix = 'gpu'
     else:
         prefix = 'cpu'
-    return df.to_csv(f'../data/{prefix}_metrics.csv', index=False)
+    
+    if chart:
+        plt.savefig(f'../data/charts/{prefix}/{random.randint(1, 100)}.png')
+    else:
+        df.to_csv(f'../data/metrics.csv', index=False)
+    
+    return 
 
 def merge_two_dicts(x, y):
     z = x.copy()
@@ -100,20 +114,26 @@ if __name__ == '__main__':
     random_select = True
     write = True
     gpu = True
-    n = 1
+    num_questions = 1
+    num_asks = 10 
     models = get_models()
     questions = questions.get_questions()
+    both = True
 
-    if n > len(questions):
+    if num_questions > len(questions):
         print('n is too big, only {len(questions)}')
         exit()
 
     if random_select:
-        questions = random.sample(questions, k=n)
+        questions = random.sample(questions, k=num_questions)
    
-    # run on the gpu 
-    metrics = run(models, questions, gpu, graph=True, loops=1, kind='box')
+    metrics = run(models, questions, gpu, write, graph=True, asks=num_asks, display=['box', 'bar'])
     
+    # run on the opposite 
+    if both:
+        gpu = not gpu
+        metrics = pd.concat([run(models, questions, gpu, write, graph=True, asks=num_asks, display=['box', 'bar']), metrics])
+   
     if write:
         save(metrics, gpu) 
 
